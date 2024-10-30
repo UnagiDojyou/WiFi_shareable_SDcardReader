d517935c16afed49e9bffb6396846ce52b5aa473
Unagi Dojyou <unagidojyou@unagidojyou.com>
2024-10-31 01:54:02 +0900

    Task based access control (#2)
    * Only ESP32S3
    
    When the settings are changed on the web side, they are not applied to the USB side.
    
    * preliminary fix for USB update
    
    * Not working
    
    Compile is OK
    Not reloaded by Windows Explorer.
    
    * Works better
    
    * add USB_REFRESH
    
    * work well
    
    LED is not work
    
    * RaspberryPiPico partial support
    
    * Rollback RaspberryPiPicoW algorithm
    
    task based access control is not adapt raspberry pi pico. because of it is not RTOS.
    
    * some fix

---
+++ WiFi_shareable_SDcardReader_ESP32S2S3/WiFi_shareable_SDcardReader_ESP32S2S3.ino
@@ -0,0 +1,249 @@
+// For ESP32 S2 and ESP32 S3
+// UnagiDojyou https://unagidojyou.com
+
+// ---------------user definition----------------------
+/*
+  What is USB_REFRESH?
+USB_REFRESH disconnects the storage from the host for a moment.
+When USB is connected to Windows and you change files via WiFi, the changes are not applied to the Windows side.
+Becase of Windows has cache of file list. When the storage is reconnected, Windows refresh cache. So, USB_REFRESH is need.
+
+If neither SCSI_REFRESH nor USB_REFRESH is selected, no USB_REFRESH is occur.
+*/
+#define SCSI_REFRESH // (recommend) Temporarily disconnect the SCSI storage.
+// #define USB_REFRESH  // Temporarily disable the USB.
+
+#define REFRESH_TIME_LENGTH 2000  // length of disconnection time (ms)
+
+#define WIFI_BUTTON 0  // GPIO of WiFi reset button
+
+#define WIFI_LED 48
+#define SD_LED 48
+
+#define CP_BOARDNAME "shareableSDReader"
+#define CP_HTMLTITLE "WiFi Setting"
+// -----------------------------------------------------
+
+#if !SOC_USB_OTG_SUPPORTED || ARDUINO_USB_MODE
+#error Device does not support USB_OTG or native USB CDC/JTAG is selected
+#endif
+
+#if defined SCSI_REFRESH && defined USB_REFRESH
+#error Please select only one of SCSI_REFRESH and USB_REFRESH.
+#endif
+
+#include <SD.h>
+#include <USB.h>
+#include "USBMSC.h"
+#include <WiFi.h>
+#include "CheckAndResponse.h"
+#include <esp_system.h>
+#include <Captive_Portal_WiFi_connector.h>
+#include <LittleFS.h>
+
+CPWiFiConfigure CPWiFi(WIFI_BUTTON, WIFI_LED, Serial);
+WiFiServer server(80);
+USBMSC msc;
+
+uint16_t updateCount = 0;
+bool POSTflagd = false;
+
+static int32_t onWrite(uint32_t lba, uint32_t offset, uint8_t *buffer, uint32_t bufsize) {
+  updateCount = 0;
+  uint32_t secSize = SD.sectorSize();
+  if (!secSize) {
+    return false;  // disk error
+  }
+  log_v("Write lba: %ld\toffset: %ld\tbufsize: %ld", lba, offset, bufsize);
+  for (int x = 0; x < bufsize / secSize; x++) {
+    uint8_t blkbuffer[secSize];
+    memcpy(blkbuffer, (uint8_t *)buffer + secSize * x, secSize);
+    if (!SD.writeRAW(blkbuffer, lba + x)) {
+      return false;
+    }
+  }
+  return bufsize;
+}
+
+static int32_t onRead(uint32_t lba, uint32_t offset, void *buffer, uint32_t bufsize) {
+  updateCount = 0;
+  uint32_t secSize = SD.sectorSize();
+  if (!secSize) {
+    return false;  // disk error
+  }
+  log_v("Read lba: %ld\toffset: %ld\tbufsize: %ld\tsector: %lu", lba, offset, bufsize, secSize);
+  for (int x = 0; x < bufsize / secSize; x++) {
+    if (!SD.readRAW((uint8_t *)buffer + (x * secSize), lba + x)) {
+      return false;  // outside of volume boundary
+    }
+  }
+  return bufsize;
+}
+
+static bool onStartStop(uint8_t power_condition, bool start, bool load_eject) {
+  log_i("Start/Stop power: %u\tstart: %d\teject: %d", power_condition, start, load_eject);
+  return true;
+}
+
+static void usbEventCallback(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+  if (event_base == ARDUINO_USB_EVENTS) {
+    arduino_usb_event_data_t *data = (arduino_usb_event_data_t *)event_data;
+    switch (event_id) {
+      case ARDUINO_USB_STARTED_EVENT:
+        Serial.println("USB PLUGGED");
+        POSTflagd = false;
+        break;
+      case ARDUINO_USB_STOPPED_EVENT: Serial.println("USB UNPLUGGED"); break;
+      case ARDUINO_USB_SUSPEND_EVENT: Serial.printf("USB SUSPENDED: remote_wakeup_en: %u\n", data->suspend.remote_wakeup_en); break;
+      case ARDUINO_USB_RESUME_EVENT: Serial.println("USB RESUMED"); break;
+
+      default: break;
+    }
+  }
+}
+
+void startUSB(void *pvParameters) {
+#ifdef USB_REFRESH
+  delay(REFRESH_TIME_LENGTH);
+#endif
+  Serial.println("Initializing MSC");
+  // Initialize USB metadata and callbacks for MSC (Mass Storage Class)
+  USB.usbPower(100);     // 100mA
+  USB.usbAttributes(0);  // no Self powered
+  msc.vendorID("ESP32");
+  msc.productID("USB_MSC");
+  msc.productRevision("1.0");
+  msc.onRead(onRead);
+  msc.onWrite(onWrite);
+  msc.onStartStop(onStartStop);
+  msc.mediaPresent(true);
+  msc.begin(SD.numSectors(), SD.sectorSize());
+  POSTflagd = false;
+  USB.begin();
+  USB.onEvent(usbEventCallback);
+  vTaskDelete(NULL);
+}
+
+void startWiFi() {
+  sprintf(CPWiFi.boardName, CP_BOARDNAME);
+  sprintf(CPWiFi.htmlTitle, CP_HTMLTITLE);
+  if (!LittleFS.begin(true)) {
+    Serial.println("Fail to start LittleFS");
+  }
+  if (!CPWiFi.begin()) {
+    Serial.println("Fail to start Capitive_Portal_WiFi_configure");
+    while (true) {}
+  }
+  WiFi.begin(CPWiFi.readSSID().c_str(), CPWiFi.readPASS().c_str());
+  int count = 0;
+  bool led = false;
+  while (WiFi.status() != WL_CONNECTED) {
+    if (count > 40) {
+      Serial.println("WiFi connect Fail. reboot.");
+      ESP.restart();
+    }
+    delay(1000);  //1sencods
+    if (CPWiFi.readButton()) {
+      ESP.restart();
+    }
+    Serial.print(".");
+    if (!led) {
+      digitalWrite(WIFI_LED, HIGH);
+      led = true;
+    } else {
+      digitalWrite(WIFI_LED, LOW);
+      led = false;
+    }
+    count++;
+  }
+  digitalWrite(WIFI_LED, LOW);
+  led = false;
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.print("IP is ");
+  Serial.println(WiFi.localIP());
+}
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting Serial");
+
+  pinMode(WIFI_LED, OUTPUT);
+  pinMode(SD_LED, OUTPUT);
+
+  Serial.println("Mounting SDcard");
+  if (!SD.begin()) {
+    Serial.println("Mount Failed");
+    uint8_t count = 0;
+    while (count < 100) {
+      digitalWrite(SD_LED, HIGH);
+      delay(500);
+      digitalWrite(SD_LED, LOW);
+      delay(500);
+      count++;
+    }
+    ESP.restart();
+  }
+
+  xTaskCreate(startUSB, "startUSB", 4096, NULL, 2, NULL);
+
+  startWiFi();
+  server.begin();  //start the server
+  Serial.print("\nHTTP server started at: ");
+  Serial.println(WiFi.localIP());
+}
+
+bool wait_media_present_accessed = false;
+bool mediaPresent = true;
+
+void loop() {
+  if (CPWiFi.readButton()) {
+    ESP.restart();
+  }
+  WiFiClient client = server.available();
+  CheckAndResponse(client);
+
+#ifdef SCSI_REFRESH
+  if (updateCount >= UINT16_MAX && POSTflagd) {
+    if (!wait_media_present_accessed) {                   // first
+      media_present_accessed = false;                     // when accessed, automaticly true.
+      wait_media_present_accessed = true;                 // flag of waiting media_present_accessed to be high
+    } else if (media_present_accessed && mediaPresent) {  // second
+      msc.mediaPresent(false);
+      mediaPresent = false;
+      Serial.println("reset USB");
+      media_present_accessed = false;
+    } else if (media_present_accessed && !mediaPresent) {  // third
+      delay(REFRESH_TIME_LENGTH);
+      msc.mediaPresent(true);
+      mediaPresent = true;
+      wait_media_present_accessed = false;
+      POSTflagd = false;
+      updateCount = 0;
+    }
+  } else if (updateCount < UINT16_MAX) {
+    updateCount++;
+    if (POSTflagd && !mediaPresent) {
+      media_present_accessed = true;
+      wait_media_present_accessed = false;
+    }
+  }
+#endif
+#ifdef USB_REFRESH
+  if ((updateCount >= UINT16_MAX) && POSTflagd) {
+    if (!wait_media_present_accessed) {                   // first
+      media_present_accessed = false;                     // when accessed, automaticly true.
+      wait_media_present_accessed = true;                 // flag of waiting media_present_accessed to be high
+    } else if (media_present_accessed && mediaPresent) {  // second
+      ESP.restart();
+    }
+  } else if (updateCount < UINT16_MAX) {
+    updateCount++;
+    if (POSTflagd && !mediaPresent) {
+      media_present_accessed = true;
+      wait_media_present_accessed = false;
+    }
+  }
+#endif
+}
--- WiFi_shareable_SDcardReader_RaspberryPiPicoW/WiFi_shareable_SDcardReader_RaspberryPiPicoW.ino
+++ WiFi_shareable_SDcardReader_RaspberryPiPicoW/WiFi_shareable_SDcardReader_RaspberryPiPicoW.ino
@@ -1,307 +1,158 @@
-#include <WiFi.h>
-#include <SPI.h>
+// For Raspberry Pi Pico W
+// WiFi is Read only
+// UnagiDojyou https://unagidojyou.com
+
+// ---------------user definition----------------------
+#define WIFI_BUTTON 2  // GPIO of WiFi reset button
+
+#define WIFI_LED LED_BUILTIN
+#define SD_LED LED_BUILTIN
+
+#define CP_BOARDNAME "shareableSDReader"
+#define CP_HTMLTITLE "WiFi Setting"
+// -----------------------------------------------------
+
 #include <SdFat.h>
 #include <Adafruit_TinyUSB.h>
-#include <WebServer.h>
-#include <DNSServer.h>
-#include <FS.h>
-#include <LittleFS.h>
+#include <WiFi.h>
 #include "CheckAndResponse.h"
-
-const char* wifi_config = "/WiFi.txt";
-bool softap = true;
-bool led = false;
-int APcount = 0;
-bool startupAP = false;
-
-char baseMacChr[18] = {0};
-
-WebServer softserver(80);
-WiFiServer server(80);
-WiFiClient client;
-DNSServer dnsServer;
-
+#include <Captive_Portal_WiFi_connector.h>
 const int _MISO = 4;  // AKA SPI RX
 const int _MOSI = 7;  // AKA SPI TX
 const int chipSelect = 5;
 const int _SCK = 6;
 
-// File system on SD Card
+CPWiFiConfigure CPWiFi(WIFI_BUTTON, WIFI_LED, Serial1);
+WiFiServer server(80);
 SdFat sd;
-
 SdFile rootS;
 SdFile fileS;
-
-// USB Mass Storage object
-Adafruit_USBD_MSC usb_msc;
+Adafruit_USBD_MSC msc;
 
 bool USBworking = false;
 bool WEBworking = false;
 
-void handleRoot() {
-  char htmlForm[500];
-  snprintf(htmlForm,500,"<!DOCTYPE html>\
-<html>\
-<head>\
-<title>Raspberry Pi Pico W WiFi Setting</title>\
-<meta name=\"viewport\" content=\"width=300\">\
-</head>\
-<body>\
-<h2>Raspberry Pi Pico W WiFi Setting</h2>\
-<p>MACaddress<br>%s</p>\
-<form action=\"/submit\" method=\"POST\">\
-SSID<br>\
-<input type=\"text\" name=\"SSID\" required\">\<br>\
-Password<br>\
-<input type=\"text\" name=\"Password\" required\">\<br>\
-<input type=\"submit\" value=\"send\">\
-</form>\
-</body>\
-</html>",baseMacChr);
-  softserver.send(200, "text/html", htmlForm);
-}
-
-void handleSubmit() {
-  if (softserver.hasArg("SSID") && softserver.hasArg("Password")) {
-    String staSSID = softserver.arg("SSID");
-    String staPassword = softserver.arg("Password");
-    Serial1.println(staSSID);
-    Serial1.println(staPassword);
-
-    char htmlForm[500];
-    snprintf(htmlForm,500,"<!DOCTYPE html>\
-<html>\
-<head>\
-<title>Raspberry Pi Pico W WiFi Setting</title>\
-<meta name=\"viewport\" content=\"width=300\">\
-</head>\
-<body>\
-<h2>Raspberry Pi Pico W WiFi Setting</h2>\
-<p>MACaddress<br>%s</p>\
-<p>Attempts to connect to %s after 10 seconds.</p>\
-<p>When led blinks at 1 second intervals, Raspberry Pi Pico W is trying to connect to WiFi.<br>\
-If it continues for a long time, press and hold the reset button for more than 5 seconds and setting WiFi again.</p>\
-</body>\
-</html>",baseMacChr,staSSID.c_str());
-    softserver.send(200, "text/html", htmlForm);
-
-    File file = LittleFS.open(wifi_config, "w");
-    file.println(staSSID); //SSID
-    file.println(staPassword); //password
-    file.close();
-    delay(10000); //10seconds
-    rp2040.reboot();
-
-  } else {
-    softserver.send(200, "text/plain", "Message not received");
+void startWiFi() {
+  sprintf(CPWiFi.boardName, CP_BOARDNAME);
+  sprintf(CPWiFi.htmlTitle, CP_HTMLTITLE);
+  if (!CPWiFi.begin()) {
+    Serial1.println("Fail to start Capitive_Portal_WiFi_configure");
+    return;
   }
+  WiFi.begin(CPWiFi.readSSID().c_str(), CPWiFi.readPASS().c_str());
+  int count = 0;
+  bool led = false;
+  while (WiFi.status() != WL_CONNECTED) {
+    if (count > 20) {
+      Serial1.println("WiFi connect Fail. reboot.");
+      rp2040.reboot();
+    }
+    delay(1000);  //1sencods
+    if (CPWiFi.readButton()) {
+      rp2040.reboot();
+    }
+    Serial1.print(".");
+    if (!led) {
+      digitalWrite(WIFI_LED, HIGH);
+      led = true;
+    } else {
+      digitalWrite(WIFI_LED, LOW);
+      led = false;
+    }
+    count++;
+  }
+  digitalWrite(WIFI_LED, LOW);
+  led = false;
+  Serial1.println("");
+  Serial1.println("WiFi connected.");
+  Serial1.print("IP is ");
+  Serial1.println(WiFi.localIP());
 }
 
-void handleNotFound() {
-  String IPaddr = ipToString(softserver.client().localIP());
-  softserver.sendHeader("Location", String("http://") + IPaddr, true);
-  softserver.send(302, "text/plain", "");
-  softserver.client().stop();
-}
-
-void setup()
-{
+void setup() {
   Serial1.begin(115200);
   Serial.end();
-  // Set disk vendor id, product id and revision with string up to 8, 16, 4 characters respectively
-  usb_msc.setID("Adafruit", "SD Card", "1.0");
 
-  // Set read write callback
-  usb_msc.setReadWriteCallback(msc_read_cb, msc_write_cb, msc_flush_cb);
+  pinMode(WIFI_LED, OUTPUT);
+  pinMode(SD_LED, OUTPUT);
 
-  // Still initialize MSC but tell usb stack that MSC is not ready to read/write
-  // If we don't initialize, board will be enumerated as CDC only
-  usb_msc.setUnitReady(false);
-  usb_msc.begin();
-
-  Serial1.println("WiFi shareable SDcard Reader");
-
-  Serial1.print("\nInitializing SD card ... ");
-  Serial1.print("CS = "); Serial1.println(chipSelect);
-
+  Serial1.println("Mounting SDcard");
   SPI.setRX(_MISO);
   SPI.setTX(_MOSI);
   SPI.setSCK(_SCK);
-  
-  if ( !sd.begin(chipSelect, SD_SCK_MHZ(50)))
-  {
-    Serial1.println("initialization failed. Things to check:");
-    Serial1.println("* is a card inserted?");
-    Serial1.println("* is your wiring correct?");
-    Serial1.println("* did you change the chipSelect pin to match your shield or module?");
-    while (1) delay(1);
+  if (!sd.begin(chipSelect, SD_SCK_MHZ(50))) {
+    Serial1.println("Mount Failed");
+    uint8_t count = 0;
+    while (count < 100) {
+      digitalWrite(SD_LED, HIGH);
+      delay(500);
+      digitalWrite(SD_LED, LOW);
+      delay(500);
+      count++;
+    }
+    rp2040.reboot();
   }
 
-  // Size in blocks (512 bytes)
+  Serial1.println("Initializing MSC");
+  msc.setID("Adafruit", "SD Card", "1.0");
+  msc.setReadWriteCallback(msc_read_cb, msc_write_cb, msc_flush_cb);
+  msc.setUnitReady(true);
 #if SD_FAT_VERSION >= 20000
   uint32_t block_count = sd.card()->sectorCount();
 #else
   uint32_t block_count = sd.card()->cardSize();
 #endif
-
-  Serial1.print("Volume size (MB):  ");
-  Serial1.println((block_count/2) / 1024);
-
-  // Set disk size, SD block size is always 512
-  usb_msc.setCapacity(block_count, 512);
-
-  // MSC is ready for read/write
-  usb_msc.setUnitReady(true);
+  msc.setCapacity(block_count, 512);
+  msc.begin();
 }
 
-void setup1(){
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  if (!LittleFS.begin()) {
-    Serial1.println("SPIFFS failed, or not present");
-    return;
-  }
-  if(LittleFS.exists(wifi_config)){
-    //Setup staAP mode
-    softap = false;
-    Serial1.println("try connect to WiFi");
-    File file = LittleFS.open(wifi_config, "r");
-    String staSSID = file.readStringUntil('\n');
-    String staPassword = "";
-    if (file.available()){
-      staPassword = file.readStringUntil('\n');
-    }
-    file.close();
-    staSSID.replace("\r", "");
-    staPassword.replace("\r", "");
-    Serial1.println(staSSID);
-    Serial1.println(staPassword);
-    WiFi.begin(staSSID.c_str(), staPassword.c_str());
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(1000); //1sencods
-      readbutton();
-      Serial1.print(".");
-      if(!led && !USBworking){
-        digitalWrite(LED_BUILTIN, HIGH);
-        led = true;
-      }else if(!USBworking){
-        digitalWrite(LED_BUILTIN, LOW);
-        led = false;
-      }
-    }
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial1.println("");
-    Serial1.println("WiFi connected.");
-    server.begin();  //start the server
-    Serial1.print("\nHTTP server started at: ");
-    Serial1.println(WiFi.localIP());
-    startupAP = true;
-  }else{
-    //Setup SoftAP mode
-    softap = true;
-    Serial1.println("start AP");
-    // Get MAC address for WiFi station
-    uint8_t baseMac[6];
-    WiFi.macAddress(baseMac);
-    sprintf(baseMacChr, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
-    char softSSID[10] = {0};
-    sprintf(softSSID, "RaspberryPiPiciW-%02X%02X%02X", baseMac[3], baseMac[4], baseMac[5]);
-
-    Serial1.print("MAC: ");
-    Serial1.println(baseMacChr);
-    Serial1.print("SSID: ");
-    Serial1.println(softSSID);
-
-    WiFi.softAP(softSSID);
-    IPAddress IP = WiFi.softAPIP();
-    Serial1.print("AP IP address: ");
-    Serial1.println(IP);
-
-    softserver.on("/", HTTP_GET, handleRoot);
-    softserver.on("/submit", HTTP_POST, handleSubmit);
-    softserver.onNotFound(handleNotFound);
-
-    dnsServer.start(53, "*", IP);
-    softserver.begin();
-  }
+void setup1() {
+  delay(10);
+  startWiFi();
+  server.begin();  //start the server
+  Serial1.print("\nHTTP server started at: ");
+  Serial1.println(WiFi.localIP());
 }
+
+bool led = false;
 
 void loop() {
-  if(startupAP){
-    if(!led && (USBworking || WEBworking)){
-      digitalWrite(LED_BUILTIN, HIGH);
-      led = true;
-    }
-    else if(led && !USBworking && !WEBworking){
-      digitalWrite(LED_BUILTIN, LOW);
-      led = false;
-    }
-    delay(10);
+  if (!led && (USBworking || WEBworking)) {
+    digitalWrite(SD_LED, HIGH);
+    led = true;
+  } else if (led && !USBworking && !WEBworking) {
+    digitalWrite(SD_LED, LOW);
+    led = false;
   }
-}
-
-void readbutton(){
-  if(BOOTSEL){
-    APcount = 0;
-    while(BOOTSEL){
-      delay(10);
-      APcount++;
-      if(APcount > 500){ //5seconds
-        Serial1.println("Erase wifi_config and reboot");
-        LittleFS.remove(wifi_config);
-        rp2040.reboot();
-        break;
-      }
-    }
-  }
-  return;
+  delay(1);
 }
 
 void loop1() {
-  if(softap){
-    softserver.handleClient();
-    dnsServer.processNextRequest();
-    APcount++;
-    delay(1);
-    if(APcount > 300){//about 0.3seconds
-      if(!led && !USBworking){
-        digitalWrite(LED_BUILTIN, HIGH);
-        led = true;
-      }
-      else if(!USBworking){
-        digitalWrite(LED_BUILTIN, LOW);
-        led = false;
-      }
-      APcount = 0;
-    }
-  }else{
-    readbutton();
-    WiFiClient client = server.available();
-    if (client) {
-      Serial1.println("new client");
-      while (client.connected()) {
-        if (client.available()) {
-          char c = client.read();
-          //Serial1.write(c); //Output Request from client
-          String request = processReequest(c);
-          if (!request.equals("")) {
-            process_request(client, request);
-          }
-          //if the line is blank, the request has ended.
-          if (isBlankLine) {
-            for(int i = 0; i <= 1000; i++){ //1000*10ms
-              if (!USBworking){
-                sendHTTP(client, request);  //send HTTP response
-                WEBworking = false;
-                break;
-              }
-              if(i >= 1000){
-                Serial1.println("[Web]timeout in loop");
-              }
-              delay(10);
+  WiFiClient client = server.available();
+  if (client) {
+    Serial1.println("new client");
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        //Serial1.write(c); //Output Request from client
+        String request = processReequest(c);
+        if (!request.equals("")) {
+          process_request(client, request);
+        }
+        //if the line is blank, the request has ended.
+        if (isBlankLine) {
+          for (int i = 0; i <= 1000; i++) {  //1000*10ms
+            if (!USBworking) {
+              sendHTTP(client, request);  //send HTTP response
+              WEBworking = false;
+              break;
             }
-            break;
+            if (i >= 1000) {
+              Serial1.println("[Web]timeout in loop");
+            }
+            delay(10);
           }
+          break;
         }
       }
     }
@@ -312,13 +163,12 @@ void loop1() {
 // Callback invoked when received READ10 command.
 // Copy disk's data to buffer (up to bufsize) and
 // return number of copied bytes (must be multiple of block size)
-int32_t msc_read_cb (uint32_t lba, void* buffer, uint32_t bufsize)
-{
+int32_t msc_read_cb(uint32_t lba, void* buffer, uint32_t bufsize) {
   USBworking = true;
-  if(WEBworking){
+  if (WEBworking) {
     int i = 0;
-    while(WEBworking){
-      if(i > 100){
+    while (WEBworking) {
+      if (i > 100) {
         Serial1.println("[USB]timeout");
         USBworking = false;
         return -1;
@@ -332,31 +182,24 @@ int32_t msc_read_cb (uint32_t lba, void* buffer, uint32_t bufsize)
   bool rc;
 
 #if SD_FAT_VERSION >= 20000
-  rc = sd.card()->readSectors(lba, (uint8_t*) buffer, bufsize/512);
+  rc = sd.card()->readSectors(lba, (uint8_t*)buffer, bufsize / 512);
 #else
-  rc = sd.card()->readBlocks(lba, (uint8_t*) buffer, bufsize/512);
+  rc = sd.card()->readBlocks(lba, (uint8_t*)buffer, bufsize / 512);
 #endif
-
-  if(!rc){
-    Serial1.print("R");
-    Serial1.println(rc);
-  }
-
   USBworking = false;
 
   return rc ? bufsize : -1;
 }
 
 // Callback invoked when received WRITE10 command.
-// Process data in buffer to disk's storage and 
+// Process data in buffer to disk's storage and
 // return number of written bytes (must be multiple of block size)
-int32_t msc_write_cb (uint32_t lba, uint8_t* buffer, uint32_t bufsize)
-{
+int32_t msc_write_cb(uint32_t lba, uint8_t* buffer, uint32_t bufsize) {
   USBworking = true;
-  if(WEBworking){
+  if (WEBworking) {
     int i = 0;
-    while(WEBworking){
-      if(i > 100){
+    while (WEBworking) {
+      if (i > 100) {
         Serial1.println("[USB]timeout");
         USBworking = false;
         return -1;
@@ -365,27 +208,20 @@ int32_t msc_write_cb (uint32_t lba, uint8_t* buffer, uint32_t bufsize)
       i++;
     }
   }
-  
+
   bool rc;
 
 #if SD_FAT_VERSION >= 20000
-  rc = sd.card()->writeSectors(lba, buffer, bufsize/512);
+  rc = sd.card()->writeSectors(lba, buffer, bufsize / 512);
 #else
-  rc = sd.card()->writeBlocks(lba, buffer, bufsize/512);
+  rc = sd.card()->writeBlocks(lba, buffer, bufsize / 512);
 #endif
-
-  if(!rc){
-    Serial1.print("W");
-    Serial1.println(rc);
-  }
-  
   return rc ? bufsize : -1;
 }
 
 // Callback invoked when WRITE10 command is completed (status received and accepted by host).
 // used to flush any pending cache.
-void msc_flush_cb (void)
-{
+void msc_flush_cb(void) {
 #if SD_FAT_VERSION >= 20000
   sd.card()->syncDevice();
 #else
